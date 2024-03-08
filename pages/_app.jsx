4975f21305541991d140a5c24509f10cd5798861
Takado <ttokayev7@gmail.com>
2024-03-09 03:51:04 +0500

    Добавление шрифта в проект

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,14 @@
 import "../styles/global.css";
+import clsx from "clsx";
+import { Inter } from "next/font/google";
+
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={clsx(inter.className, "text-slate-900")}>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
